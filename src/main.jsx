c0acc92d5a7e45a331ffbdd7ac769b6f69568a4b
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2026-04-07 09:07:38 +0100

    Fix Bootstrap JS import for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import App from "./App";
 import "swiper/css";
 import "swiper/css/pagination";
 
+// ✅ Bootstrap styles and JS bundle
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
